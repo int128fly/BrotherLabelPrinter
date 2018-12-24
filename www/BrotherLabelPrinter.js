520caa79046c656b873e8fe7b474293a0505a976
Mathias Beaulieu-Duncan <thenightysfly@gmail.com>
2018-12-24 16:18:50 -0500

    bla

--- www/BrotherLabelPrinter.js
+++ www/BrotherLabelPrinter.js
@@ -1,7 +1,6 @@
-var exec = require('cordova/exec');
-
 module.export = {
     coolMethod: function (arg0, success, error) {
-        exec(success, error, 'BrotherLabelPrinter', 'coolMethod', [arg0]);
+        console.log('got into plugin call');
+        cordova.exec(success, error, 'BrotherLabelPrinter', 'coolMethod', [arg0]);
     }
 };
